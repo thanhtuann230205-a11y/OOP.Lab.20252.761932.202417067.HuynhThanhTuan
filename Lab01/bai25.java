55c3645fb8acbb9240082095bd09f69bd44a304d
Huynh Thanh Tuan <thanhtuann230205@gmail.com>
2026-03-24 20:06:09 +0700

    Bài 2.2.5

---
+++ Lab01/bai25.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+public class bai25 {
+    public static void main(String[] args){
+        String m=JOptionPane.showInputDialog(null,"Nhập số a: ");
+        String n=JOptionPane.showInputDialog(null,"Nhập số b: ");
+        double a=Double.parseDouble(m);
+        double b=Double.parseDouble(n);
+        double tổng=a+b;
+        double hiệu=a-b;
+        double tích=a*b;
+        String result="Tổng: "+tổng+" Hiệu: "+hiệu+" Tích: "+tích;
+        if(b!=0){
+            double thương=a/b;
+            result+=" Thương: "+thương;
+        }
+        else{
+            result+=" Thương: "+"Không xác định";
+        }
+        JOptionPane.showMessageDialog(null, result,"Kết quả",JOptionPane.INFORMATION_MESSAGE);
+    }
+}
