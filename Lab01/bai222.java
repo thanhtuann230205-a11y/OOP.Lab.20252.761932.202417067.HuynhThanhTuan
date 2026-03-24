52b29f67749ac22b8d6871967ef8439b990ee79e
Huynh Thanh Tuan <thanhtuann230205@gmail.com>
2026-03-24 18:37:52 +0700

    Bài 2.2.2

---
+++ Lab01/bai222.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class bai222 {
+    public static void main(String[] args){
+                JOptionPane.showMessageDialog(null, "Hello world! How are you?");
+    }
+}
