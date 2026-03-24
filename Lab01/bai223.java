461558d11764c9a3198f8a17d631cd6b8fa80c66
Huynh Thanh Tuan <thanhtuann230205@gmail.com>
2026-03-24 18:43:46 +0700

    Bài 2.2.3

---
+++ Lab01/bai223.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class bai223{
+    public static void main(String[] args){
+        String s;
+        s=JOptionPane.showInputDialog("Please enter your name: ");
+        JOptionPane.showMessageDialog(null,"Hi "+s+"!");
+    }
+}
