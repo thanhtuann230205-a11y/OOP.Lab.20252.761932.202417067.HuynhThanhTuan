c88f36289d5571c1457f12387edf785a40ff4b4e
Huynh Thanh Tuan <thanhtuann230205@gmail.com>
2026-03-27 01:25:43 +0700

    Bài 6.6

---
+++ Lab01/bai66.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class bai66 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số hàng: ");
+        int rows = sc.nextInt();
+        System.out.print("Nhập số cột: ");
+        int cols = sc.nextInt();
+        int[][] matrix1 = new int[rows][cols];
+        int[][] matrix2 = new int[rows][cols];
+        int[][] sumMatrix = new int[rows][cols];
+        System.out.println("Nhập số phần tử ma trận 1:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Nhập số phần tử ma trận 2:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix2[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sumMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+        System.out.println("Tổng hai ma trận:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(sumMatrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
